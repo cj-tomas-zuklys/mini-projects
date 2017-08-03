0ace517c17238482bcb831268c80fc9b7e2274a5
Tomas Zuklys <tomas.zuklys@gmail.com>
2017-08-03 22:29:03 +0300

    Fix: Removed SpringBootServletInitializer as it's not needed for executable jar

--- spring-boot-soapws/src/main/java/net/codinginaction/mp/sbsoapws/BottomUpApplication.java
+++ spring-boot-soapws/src/main/java/net/codinginaction/mp/sbsoapws/BottomUpApplication.java
@@ -2,16 +2,9 @@ package net.codinginaction.mp.sbsoapws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BottomUpApplication extends SpringBootServletInitializer {
-
-	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(BottomUpApplication.class);
-    }
+public class BottomUpApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BottomUpApplication.class, args);
